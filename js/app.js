297986b4c08d6eb06d72cb410d47ab6fd1a1eb08
David Bell <dbelldev@icloud.com>
2018-10-14 18:22:42 +0100

    sunday

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
 
-// const appendDataToTable = function (newTable, climbElement, distanceElement, gradientElement){
-//   newTable.appendChild(climbElement)
-//   // newTable.appendChild(distanceElement)
-//   // newTable.appendChild(gradientElement)
-//
-// const tableContents = document.querySelector('#climb');
-// tableContents.appendChild(newTable)
-// }
-
 const appendFirstCell = function (newCellOne, climbElement){
   newCellOne.appendChild(climbElement)
 const firstCell = document.querySelector('.climb');
 firstCell.appendChild(newCellOne)
+// debugger
 }
 const appendSecondCell = function (newCellTwo, distanceElement){
   newCellTwo.appendChild(distanceElement)
@@ -23,20 +15,40 @@ const appendThirdCell = function (newCellThree, gradientElement){
 const thirdCell = document.querySelector('.gradient');
 thirdCell.appendChild(newCellThree)
 }
+const appendFourthCell = function (newCellFour, tourElement){
+  newCellFour.appendChild(tourElement)
+const fourthCell = document.querySelector('.tour');
+fourthCell.appendChild(newCellFour)
+}
+// const appendFifthCell = function (newCellFive, countryElement){
+//   newCellFive.appendChild(countryElement)
+// const fifthCell = document.querySelector('.country');
+// fifthCell.appendChild(newCellFive)
+// }
 
-const createNewRow = function(climb, distance, gradient){
+const createNewRow = function(tour, climb, distance, gradient){
   const newCellOne = document.createElement('tr');
   const newCellTwo = document.createElement('tr');
   const newCellThree = document.createElement('tr');
+  const newCellFour = document.createElement('tr');
+  // const newCellFive = document.createElement('tr');
+
   const climbElement = document.createElement('td')
   climbElement.textContent = climb;
   const distanceElement = document.createElement('td')
   distanceElement.textContent = distance;
   const gradientElement = document.createElement('td')
   gradientElement.textContent = gradient;
+  const tourElement = document.createElement('td')
+  tourElement.textContent = tour;
+  // const countryElement = document.createElement('td')
+  // tourElement.textContent = country;
+
   appendFirstCell(newCellOne, climbElement);
   appendSecondCell(newCellTwo, distanceElement);
   appendThirdCell(newCellThree, gradientElement);
+  appendFourthCell(newCellFour, tourElement);
+  // appendFifthCell(newCellFive, countryElement);
 }
 
 // const createNewRow = function(climb, distance, gradient){
@@ -53,19 +65,31 @@ const createNewRow = function(climb, distance, gradient){
 const handleSubmit = function(event){
   event.preventDefault();
   console.log("Javascrpt!");
+  const tour = event.target.options.value;
   const climb = event.target.climb.value;
   const distance = event.target.distance.value;
   const gradient = event.target.gradient.value;
+  // const country = event.target.country.value;
   // debugger
-  createNewRow(climb, distance, gradient);
+  // console.dir(event)
+  createNewRow(tour, climb, distance, gradient);
 
   const form = event.target;
   form.reset();
 }
 
+const handleDelete = function(event){
+  const climbTable = document.querySelector('#table-input');
+  while(climbTable.firstChild){
+    climbTable.removeChild(climbTable.firstChild);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   // console.log("JavaScript!");
 const form = document.querySelector('#form-input');
-// debugger
 form.addEventListener('submit', handleSubmit);
+
+const deleteButton = document.querySelector('#deleteAll');
+deleteButton.addEventListener('click', handleDelete);
 })
